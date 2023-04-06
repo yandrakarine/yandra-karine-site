98949f67791a1dbc72253db4ca0584c985d174a5
yandrakarine <yandrakarine.dev@outlook.com>
2023-04-06 14:35:00 -0300

    create styled-components for Menu

---
+++ src/components/Menu/style.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MenuList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const MenuItem = styled.li`
+  display: flex;
+  padding: 1rem;
+`;
