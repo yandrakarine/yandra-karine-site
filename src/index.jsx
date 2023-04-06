685e50741a7e8e186c023e092ea0e7a8e9c96efe
yandrakarine <yandrakarine.dev@outlook.com>
2023-04-06 14:38:11 -0300

    import fontawesome

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./main/App";
+import "./fontawesome";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
