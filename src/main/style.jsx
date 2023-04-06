9f0bfb91c024f3d18f406194178f2b8813653a68
yandrakarine <yandrakarine.dev@outlook.com>
2023-04-06 14:36:30 -0300

    create styled-components Main and Content

---
+++ src/main/style.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { Container } from "../components/common";
+
+export const Main = styled(Container)`
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+`;
+
+export const Content = styled(Container)`
+  width: 100%;
+  height: 100%;
+  background: #fff;
+`;
