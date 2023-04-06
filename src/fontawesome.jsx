8b68671a81bfeb38cf32ffe52f36597104714b38
yandrakarine <yandrakarine.dev@outlook.com>
2023-04-06 14:37:54 -0300

    install and import fontawesome lib

---
+++ src/fontawesome.jsx
@@ -0,0 +1,11 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {
+  faHouse,
+  faFlask,
+  faLaptopCode,
+  faBriefcase,
+} from "@fortawesome/free-solid-svg-icons";
+
+// adicionado na lib
+library.add(faGithub, faLinkedin, faHouse, faFlask, faLaptopCode, faBriefcase);
