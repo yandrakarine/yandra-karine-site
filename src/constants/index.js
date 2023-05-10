0075387565f7ffea8c9974f816f07e3e93c1c19a
yandrakarine <yandrakarine.dev@outlook.com>
2023-05-10 18:35:40 -0300

    create file for constants

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const RoutesENUM = {
+    HOME: "/home",
+    EXPERIENCE: "/experience",
+    CODE_SKILLS: "/code-skills",
+}
+
+export const Contacts = {
+    LINKEDIN_URL: "https://www.linkedin.com/in/yandra-karine-4183b4143/",
+    GITHUB_URL: "https://github.com/yandrakarine"
+}
