1ef5fd50afd932cd0f62937948845e82e5f697a0
yandrakarine <yandrakarine.dev@outlook.com>
2023-05-10 18:37:27 -0300

    apply components in App.jsx

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,18 +1,26 @@
-import { Sidebar } from "../components/Sidebar";
-import GlobalStyle from "../styles/globalStyles";
-import { Content, Main } from "./style";
-import "../fontawesome";
+import { BrowserRouter } from 'react-router-dom';
+import { Sidebar } from '../components/Sidebar';
+import GlobalStyle from '../styles/globalStyles';
+import { Content, Main } from './style';
+import { RootRouter } from '../routes';
+import '../fontawesome';
 
+/* Configurar o ESLint + Prettier no projeto pra testar */
+/* algo não está sendo usado?? -  retirar */
+/* Criar os componentes da Home */
+/* Criar os componentes da Experiences */
+/* Criar os componentes da code-skills */
 function App() {
   return (
-    <>
+    <BrowserRouter basename='/'>
       <GlobalStyle />
       <Main>
-        <Sidebar>
-          <Content></Content>
-        </Sidebar>
+        <Sidebar />
+        <Content>
+          <RootRouter />
+        </Content>
       </Main>
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/main/style.jsx
+++ src/main/style.jsx
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { Container } from "../components/common";
+import styled from 'styled-components';
+import { Container } from '../components/common';
 
 export const Main = styled(Container)`
   width: 100vw;
@@ -11,5 +11,5 @@ export const Main = styled(Container)`
 export const Content = styled(Container)`
   width: 100%;
   height: 100%;
-  background: #fff;
+  background: #f9f7fd; // vai mudar de acordo com o tema
 `;
