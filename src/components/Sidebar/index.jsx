fbaa72686f7b1812fe56c34a73de9ea9947d62d4
yandrakarine <yandrakarine.dev@outlook.com>
2023-05-10 18:23:52 -0300

    create Sidebar component

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,5 +1,6 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Menu } from "../Menu/index";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Menu } from '../Menu/index';
+import { Contacts, RoutesENUM } from '../../constants';
 import {
   Wrapper,
   Profile,
@@ -8,47 +9,58 @@ import {
   MyOccupation,
   BottomContainer,
   ContactRow,
-} from "./style";
+} from './style';
+import { LinkContainer } from '../common';
 
 export const Sidebar = () => {
   const items = [
     {
-      itemIcon: "fa-solid fa-house",
-      itemRoute: "RoutesENUM.HOME",
-      itemTitle: "home",
+      itemIcon: 'fa-solid fa-house',
+      itemRoute: RoutesENUM.HOME,
+      itemTitle: 'home',
     },
     {
-      itemIcon: "fa-solid fa-briefcase",
-      itemRoute: "RoutesENUM.EXPERIENCE",
-      itemTitle: "experiences",
+      itemIcon: 'fa-solid fa-briefcase',
+      itemRoute: RoutesENUM.EXPERIENCE,
+      itemTitle: 'experiences',
     },
     {
-      itemIcon: "fa-solid fa-laptop-code",
-      itemRoute: "RoutesENUM.CODE_SKILLS",
-      itemTitle: "skills",
+      itemIcon: 'fa-solid fa-laptop-code',
+      itemRoute: RoutesENUM.CODE_SKILLS,
+      itemTitle: 'skills',
     },
   ];
+
+  const { LINKEDIN_URL, GITHUB_URL } = Contacts;
+
   return (
     <Wrapper>
       <Profile>
         <MyAvatar />
-        <MyName children="Yandra Karine" />
-        <MyOccupation children="Fluffy-stuff Developer" />
+        <MyName children='Yandra Karine' />
+        <MyOccupation children='Fluffy-stuff Developer' />
       </Profile>
       <Menu menuItemsList={items} />
       <BottomContainer>
         <ContactRow>
-          {/* depois é preciso colocar os links */}
-          <FontAwesomeIcon
-            icon="fa-brands fa-linkedin"
-            style={{ color: "white" }} // aqui vai mudar de acordo com o theme(ligth:dark)
-            size="xl"
-          />
-          <FontAwesomeIcon
-            icon="fa-brands fa-github"
-            style={{ color: "white" }} // aqui vai mudar de acordo com o theme(ligth:dark)
-            size="xl"
-          />
+          <LinkContainer href={LINKEDIN_URL}>
+            <FontAwesomeIcon
+              icon='fa-brands fa-linkedin'
+              style={{ color: 'white' }} // aqui vai mudar de acordo com o theme(ligth:dark)
+              size='2x'
+              cursor='pointer'
+              title='Linkedin'
+            />
+          </LinkContainer>
+          <LinkContainer href={GITHUB_URL}>
+            <FontAwesomeIcon
+              icon='fa-brands fa-github'
+              style={{ color: 'white' }} // aqui vai mudar de acordo com o theme(ligth:dark)
+              size='2x'
+              cursor='pointer'
+              title='Github'
+            />
+          </LinkContainer>
         </ContactRow>
       </BottomContainer>
     </Wrapper>
--- src/components/Sidebar/style.jsx
+++ src/components/Sidebar/style.jsx
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { Container } from "../common";
+import styled from 'styled-components';
+import { Container } from '../common';
 
 export const Wrapper = styled.nav`
   display: flex;
@@ -7,7 +7,7 @@ export const Wrapper = styled.nav`
   justify-content: space-between;
   align-content: center;
   width: 9rem;
-  background: #9b4f8a;
+  background: #2f2b55;
   padding: 2rem 0;
 `;
 // background: #9b4f8a;
@@ -22,7 +22,7 @@ export const Profile = styled(Container)`
 
 export const MyAvatar = styled(Container)`
   border-radius: 50%;
-  background-image: url("https://i.pinimg.com/736x/1c/ca/24/1cca24df3c875c58cf0a872350637007.jpg");
+  background-image: url('https://i.pinimg.com/736x/1c/ca/24/1cca24df3c875c58cf0a872350637007.jpg');
   width: 5rem;
   height: 5rem;
   background-size: contain;
