b9345cbfb2f7029214407a17ae869da1de8a3300
yandrakarine <yandrakarine.dev@outlook.com>
2023-05-12 10:02:42 -0300

    add new icons to lib

--- src/fontawesome.jsx
+++ src/fontawesome.jsx
@@ -1,11 +1,39 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faGithub,
+  faLinkedin,
+  faReact,
+  faJsSquare,
+  faGitAlt,
+  faCss3Alt,
+  faHtml5,
+} from '@fortawesome/free-brands-svg-icons';
 import {
   faHouse,
   faFlask,
   faLaptopCode,
   faBriefcase,
-} from "@fortawesome/free-solid-svg-icons";
+  faFileCode,
+  faScrewdriverWrench,
+  faRobot,
+  faPeopleGroup,
+} from '@fortawesome/free-solid-svg-icons';
 
 // adicionado na lib
-library.add(faGithub, faLinkedin, faHouse, faFlask, faLaptopCode, faBriefcase);
+library.add(
+  faGithub,
+  faLinkedin,
+  faHouse,
+  faFlask,
+  faLaptopCode,
+  faBriefcase,
+  faReact,
+  faJsSquare,
+  faGitAlt,
+  faCss3Alt,
+  faHtml5,
+  faFileCode,
+  faScrewdriverWrench,
+  faRobot,
+  faPeopleGroup,
+);
