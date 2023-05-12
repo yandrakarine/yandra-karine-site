626727e9ea7c1250f06f66a3fc0d1b83114bbaba
yandrakarine <yandrakarine.dev@outlook.com>
2023-05-12 10:03:43 -0300

    change the base color to Container

--- src/components/common/index.jsx
+++ src/components/common/index.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  color: #000;
+  color: #2f2b55;
 `;
 
 export const LinkContainer = styled.a.attrs(() => ({
