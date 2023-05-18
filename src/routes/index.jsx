73c9ef70c262009b490f7d70c7975b8ab9019437
yandrakarine <yandrakarine.dev@outlook.com>
2023-05-18 16:02:37 -0300

    chore: add default Route to Home

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { RoutesENUM } from '../constants';
 import { CodeSkillsPage } from '../pages/CodeSkills';
 import { ExperiencesPage } from '../pages/Experiences';
 import { HomePage } from '../pages/Home';
 
 export function RootRouter() {
-  console.log('ta renderizando rootRouter');
   return (
     <Routes>
       <Route element={<HomePage />} exact path={RoutesENUM.HOME} />
-      <Route element={<CodeSkillsPage />} path={RoutesENUM.CODE_SKILLS} />
-      <Route element={<ExperiencesPage />} path={RoutesENUM.EXPERIENCE} />
+      <Route element={<CodeSkillsPage />} exact path={RoutesENUM.CODE_SKILLS} />
+      <Route element={<ExperiencesPage />} exact path={RoutesENUM.EXPERIENCE} />
+      <Route path='/' element={<Navigate replace to='/home' />} />
     </Routes>
   );
 }
