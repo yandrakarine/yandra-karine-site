33bf7e43e0b25087faceaebd5b7f403ebeca43ab
yandrakarine <yandrakarine.dev@outlook.com>
2023-05-18 16:02:11 -0300

    style: turn Main view reponsive

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -5,11 +5,6 @@ import { Content, Main } from './style';
 import { RootRouter } from '../routes';
 import '../fontawesome';
 
-/* Configurar o ESLint + Prettier no projeto pra testar */
-/* algo não está sendo usado?? -  retirar */
-/* Criar os componentes da Home */
-/* Criar os componentes da Experiences */
-/* Criar os componentes da code-skills */
 function App() {
   return (
     <BrowserRouter basename='/'>
--- src/main/style.jsx
+++ src/main/style.jsx
@@ -1,15 +1,22 @@
 import styled from 'styled-components';
 import { Container } from '../components/common';
+import { device } from '../styles/devices';
 
 export const Main = styled(Container)`
   width: 100vw;
   height: 100vh;
   margin: 0;
   padding: 0;
+
+  @media (${device.mobileL}) {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
 `;
 
 export const Content = styled(Container)`
   width: 100%;
   height: 100%;
-  background: #f9f7fd;
+  background-color: #f9f7fd;
 `;
