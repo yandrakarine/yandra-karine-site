7cd4756b6d74b27574ec3535a84f170e573af45d
yandrakarine <yandrakarine.dev@outlook.com>
2023-05-18 16:03:27 -0300

    create devices file

---
+++ src/styles/devices.js
@@ -0,0 +1,23 @@
+const size = {
+    mobileS: '320px',
+    mobileM: '375px',
+    mobileL: '438px',
+    tablet: '768px',
+    laptopS: '1024px',
+    laptopM: '1440px',
+    desktopM: '1080px',
+    desktopL: '2560px',
+  }
+  
+  export const device = {
+    mobileS: `max-width: ${size.mobileS}`,
+    mobileM: `max-width: ${size.mobileM}`,
+    mobileL: `max-width: ${size.mobileL}`,
+    tablet: `max-width: ${size.tablet}`,
+    laptopS: `max-width: ${size.laptopS}`,
+    laptopM: `max-width: ${size.laptopM}`,
+    desktopM: `max-width: ${size.desktopM}`,
+    desktopL: `max-width: ${size.desktopL}`
+  };
+  
+  
