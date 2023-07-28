7cf29687b43c9023bfff84d1aa10c2e8e8efe627
yandrakarine <yandrakarine.dev@outlook.com>
2023-07-28 10:16:09 -0300

    add route to settings page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,13 +4,16 @@ import { RoutesENUM } from '../constants';
 import { CodeSkillsPage } from '../pages/CodeSkills';
 import { ExperiencesPage } from '../pages/Experiences';
 import { HomePage } from '../pages/Home';
+import { Settigs } from '../pages/settings';
 
 export function RootRouter() {
   return (
     <Routes>
       <Route element={<HomePage />} exact path={RoutesENUM.HOME} />
+      <Route element={<ExperiencesPage />} exact path={RoutesENUM.EXPERIENCE} />
       <Route element={<CodeSkillsPage />} exact path={RoutesENUM.CODE_SKILLS} />
       <Route element={<ExperiencesPage />} exact path={RoutesENUM.EXPERIENCE} />
+      <Route element={<Settigs />} exact path={RoutesENUM.SETTINGS} />
       <Route path='/' element={<Navigate replace to='/home' />} />
     </Routes>
   );
