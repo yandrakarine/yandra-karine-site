895259099b8bdc1be51515af3aea22ce26ef86f6
yandrakarine <yandrakarine.dev@outlook.com>
2023-07-31 15:39:48 -0300

    add dark theme for Settings page

--- src/pages/settings/index.jsx
+++ src/pages/settings/index.jsx
@@ -2,13 +2,26 @@ import { i18n } from '../../translate/i18n';
 import { Idiom, i18nKeys } from '../../constants';
 import brazilIcon from '../../assets/icons/brazil.svg';
 import usaIcon from '../../assets/icons/united-states.svg';
-import { CountryFlagSettings, SettingContainer, SettingsTitle, ToolsTitle, Wrapper } from './style';
+import sunIcon from '../../assets/icons/sun.svg';
+import moonIcon from '../../assets/icons/moon.svg';
+import { useTheme } from '../../context/theme';
+
+import {
+  CountryFlagSettings,
+  Icon,
+  IconRow,
+  SettingContainer,
+  SettingsTitle,
+  ToolsTitle,
+  Wrapper,
+} from './style';
 import { TranslateRow } from '../../components/Sidebar/style';
 
 const { settingsPage } = i18nKeys;
 
 export const Settigs = () => {
   const { enKey, ptKey, storageKey } = Idiom;
+  const { changeTheme } = useTheme();
 
   const changeIdiom = ({ newIdiomKey }) => {
     const currentIdiomKey = localStorage.getItem(`${storageKey}`);
@@ -34,6 +47,11 @@ export const Settigs = () => {
             onClick={() => changeIdiom({ newIdiomKey: enKey })}
           />
         </TranslateRow>
+        <ToolsTitle children={i18n.t(settingsPage.theme)} />
+        <IconRow>
+          <Icon onClick={() => changeTheme('light')} src={sunIcon} />
+          <Icon onClick={() => changeTheme('dark')} src={moonIcon} />
+        </IconRow>
       </SettingContainer>
     </Wrapper>
   );
