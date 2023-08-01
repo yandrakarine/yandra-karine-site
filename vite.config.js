3fc3f2d252f56d3dd8a3035d3b335235d2f0a29d
yandrakarine <yandrakarine.dev@outlook.com>
2023-08-01 13:20:30 -0300

    fix:build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'yandra-karine-site',
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: 'yandra-karine-site',
+//   plugins: [react()],
+// })
